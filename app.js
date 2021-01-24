470fb7a0a4de52ff449e79ec44569dc76095dc04
Alexey <redspirit@live.ru>
2021-01-24 13:35:18 +0300

    show corrent position

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
 const proxy = require("node-tcp-proxy");
 const mc = require('minecraft-protocol');
-const packetNames = require('./packet-names.js').packets;
 
 const snifferPort = 9000;
 // const remoteServer = 'dog.satoko.org';
 const remoteServer = 'localhost';
 const remotePort = 25565;
-const version = '1.16.4';
-
-const states = mc.states;
-
-//return console.log(mc.supportedVersions);
-// HANDSHAKING: 'handshaking',
-// STATUS: 'status',
-// LOGIN: 'login',
-// PLAY: 'play'
-
+const version = '1.16.5';
 
 proxy.createProxy(snifferPort, remoteServer, remotePort, {
     upstream: (context, data) => {
-
-        //console.log('To server:', data);
         handleData(true, data);
-
         return data;
     },
     downstream: (context, data) => {
-
-        //console.log('To client:', data);
-        //handleData(false, data);
-
         return data;
     }
 });
 console.log(`Proxy started on ${snifferPort}`);
 
+let parser = mc.createDeserializer({
+    state: mc.states.PLAY,
+    isServer: true,
+    version
+});
 
-let handleData = (toServer, buffer) => {
+let oldPos = '';
 
-    let parser = mc.createDeserializer({
-        state: states.PLAY,
-        isServer: toServer,
-        //packetsToParse: {"packet": true},
-        version
-    });
-
+function handleData(toServer, buffer) {
 
     try {
-        let result = parser.parsePacketBuffer(buffer).data;
-        console.log(result);
+        let result = parser.parsePacketBuffer(buffer.slice(2)).data;
+
+        //console.log(result);
+
+        if(result.name === 'position') {
+
+            let p = [
+                Math.round(result.params.x),
+                Math.round(result.params.y),
+                Math.round(result.params.z),
+            ];
+
+            let strPos = p.join(',');
+
+            if(strPos !== oldPos) {
+                console.log(`X=${p[0]}, Y=${p[1]}, Z=${p[2]}`);
+                oldPos = strPos;
+            }
+
+        }
+
     } catch (e) {
-        console.log('oops');
+
     }
 
-    // parser.on('error', (err) => {
-    //     console.log('err', err);
-    // });
-    //
-    // parser.on('data', (result) => {
-    //     console.log('data', result);
-    // });
-
-};
+}
--- p_proxy.js
+++ p_proxy.js
@@ -1,68 +1,12 @@
 const mc = require('minecraft-protocol')
 
 const states = mc.states
-function printHelpAndExit (exitCode) {
-    console.log('usage: node proxy.js [<options>...] <target_srv> <version>')
-    console.log('options:')
-    console.log('  --dump name')
-    console.log('    print to stdout messages with the specified name.')
-    console.log('  --dump-all')
-    console.log('    print to stdout all messages, except those specified with -x.')
-    console.log('  -x name')
-    console.log('    do not print messages with this name.')
-    console.log('  name')
-    console.log('    a packet name as defined in protocol.json')
-    console.log('examples:')
-    console.log('  node proxy.js --dump-all -x keep_alive -x update_time -x entity_velocity -x rel_entity_move -x entity_look -x entity_move_look -x entity_teleport -x entity_head_rotation -x position localhost 1.8')
-    console.log('    print all messages except for some of the most prolific.')
-    console.log('  node examples/proxy.js --dump open_window --dump close_window --dump set_slot --dump window_items --dump craft_progress_bar --dump transaction --dump close_window --dump window_click --dump set_creative_slot --dump enchant_item localhost 1.8')
-    console.log('    print messages relating to inventory management.')
-
-    process.exit(exitCode)
-}
-
-if (process.argv.length < 4) {
-    console.log('Too few arguments!')
-    printHelpAndExit(1)
-}
-
-process.argv.forEach(function (val) {
-    if (val === '-h') {
-        printHelpAndExit(0)
-    }
-})
 
 const args = process.argv.slice(2)
-let host
+let host = 'localhost'
 let port = 25565
-let version
+let version = '1.16.5'
 
-let printAllNames = false
-const printNameWhitelist = {}
-const printNameBlacklist = {};
-(function () {
-    let i = 0
-    for (i = 0; i < args.length; i++) {
-        const option = args[i]
-        if (!/^-/.test(option)) break
-        if (option === '--dump-all') {
-            printAllNames = true
-            continue
-        }
-        i++
-        const name = args[i]
-        if (option === '--dump') {
-            printNameWhitelist[name] = 'io'
-        } else if (option === '-x') {
-            printNameBlacklist[name] = 'io'
-        } else {
-            printHelpAndExit(1)
-        }
-    }
-    if (!(i + 2 <= args.length && args.length <= i + 4)) printHelpAndExit(1)
-    host = args[i++]
-    version = args[i++]
-})()
 
 if (host.indexOf(':') !== -1) {
     port = host.substring(host.indexOf(':') + 1)
@@ -71,7 +15,7 @@ if (host.indexOf(':') !== -1) {
 
 const srv = mc.createServer({
     'online-mode': false,
-    port: 25566,
+    port: 9000,
     keepAlive: false,
     version: version
 })
@@ -103,7 +47,9 @@ srv.on('login', function (client) {
 
             if(meta.name === 'position') {
 
-                console.log('client->server:', client.state + '.' + meta.name + ' :', JSON.stringify(data))
+                //console.log('meta', meta);
+
+                //console.log('client->server:', client.state + '.' + meta.name + ' :', data)
 
             }
 
@@ -147,8 +93,12 @@ srv.on('login', function (client) {
     })
     client.on('raw', function (buffer, meta) {
         if (meta.state !== states.PLAY || targetClient.state !== states.PLAY) { return }
-        const packetData = client.deserializer.parsePacketBuffer(buffer).data.params
-        const packetBuff = targetClient.serializer.createPacketBuffer({ name: meta.name, params: packetData })
+
+        console.log('>>>', buffer);
+        const packetData = client.deserializer.parsePacketBuffer(buffer).data.params;
+        console.log('>>>', packetData);
+
+        const packetBuff = targetClient.serializer.createPacketBuffer({ name: meta.name, params: packetData });
         if (!bufferEqual(buffer, packetBuff)) {
             console.log('client->server: Error in packet ' + meta.state + '.' + meta.name)
             console.log('received buffer', buffer.toString('hex'))
